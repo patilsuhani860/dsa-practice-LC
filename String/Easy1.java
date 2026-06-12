b0eff89571f6c40199adfdfe1d4ef4f7fdc72b7f
patilsuhani860 <patilsuhani860@gmail.com>
2026-06-12 15:52:54 +0530

    converted roman numerals to integer

---
+++ String/Easy1.java
@@ -0,0 +1,37 @@
+class Easy1 {
+    public int romanToInt(String s) {
+        int n=s.length();
+        int result = 0;
+        for(int i=0; i<n; i++){
+            char c = s.charAt(i);
+            if(c=='I'){
+                if(i+1<n && (s.charAt(i+1)=='V' || s.charAt(i+1)=='X')){
+                    result-=1;
+                }else{
+                    result+=1;
+                }
+            }else if(c=='V'){
+                result+=5;
+            }else if(c=='X'){
+                if(i+1<n && (s.charAt(i+1)=='L' || s.charAt(i+1)=='C')){
+                    result-=10;
+                }else{
+                    result+=10;
+                }
+            }else if(c=='L'){
+                result+=50;
+            }else if(c=='C'){
+                if(i+1<n && (s.charAt(i+1)=='D' || s.charAt(i+1)=='M')){
+                    result-=100;
+                }else{
+                    result+=100;
+                }
+            }else if(c=='D'){
+                result+=500;
+            }else if(c=='M'){
+                result+=1000;
+            }
+        }
+        return result;
+    }
+}
